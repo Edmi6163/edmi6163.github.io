7368d76ee7c3552d7982d0194eb5a0137d777bc6
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-10-06 17:55:47 +0200

    small updates

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/sveltekit' : ''
+    }
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
